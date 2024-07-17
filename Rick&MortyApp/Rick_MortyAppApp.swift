648acda2917f2ac5f922d6ac754aab9e041f52b5
Tesloboy <meex_v92@mail.ru>
2024-07-17 12:33:19 +0300

    Initial Commit

---
+++ Rick&MortyApp/Rick_MortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyAppApp.swift
+//  Rick&MortyApp
+//
+//  Created by Viktor Teslenko on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
